6d7c24fc0ddda21fb54ff86dd7bce037d3da9581
Kyle Gracey <kyle@kylegracey.com>
2017-04-29 16:05:19 -0500

    Laying out a plan
    Hopefully more efficient than the original.

--- app.js
+++ app.js
@@ -14,10 +14,24 @@ function parseCB(err, data) {
       quote : '"'
     };
     let jsonData = csvjson.toObject(data, csvjsonOptions);
+    let jsonOutput = [];
 
+    // Loop through each object in the jsonData
     jsonData.forEach(function(obj) {
-      console.log(obj.SourceFile);
+      // Put keywords into a working array
+      let KeywordArr = elm.Keywords.split(', ');
+
+      // Search through keywords for matches and pull them out into their own separate metaproperties
+
+      // Organize and output metaproperties in the correct order
+
+      // Push object into output array
+
     });
+
+    // Convert jsonOutput back to CSV
+
+    // Write CSV to output file
 
   }
 }
