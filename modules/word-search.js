e681030097638d35ace8cee84109185bae6c049d
Kyle Gracey <kyle@kylegracey.com>
2017-05-01 18:19:04 -0500

    Getting alt term remapping set up
    still figuring out the best way to search for the terms.

--- modules/word-search.js
+++ modules/word-search.js
@@ -15,6 +15,11 @@ module.exports = function wordSearch(objInput, objOutput, KeywordCats){
   let newObjTags = [];
   let objKeywords = objInput.Keywords.split(", ");
 
+  // List of Keywords to Re-map
+  const altKeywords = {
+    "Whey Protein Bar" : ["Recover Bar", "Rec Bar"]
+  };
+
   for (let key in KeywordCats) {
     const catName = key.toLowerCase().replace(/\s/g, '');
     const catSettingArr = KeywordCats[key];
@@ -25,8 +30,25 @@ module.exports = function wordSearch(objInput, objOutput, KeywordCats){
       for (let objKey in objKeywords){
         if (keyword === objKeywords[objKey]) {
           //Found an exact match!
+          console.log('Found exact match for ' + catName + ' with ' + objKeywords[objKey]);
           catArrOutput.push(objKeywords[objKey]);
           objKeywords.splice(objKey, 1);
+          break;
+        } else if (keyword !== objKeywords[objKey]) {
+          // Check alt keywords
+          console.log("Didn't find match for " + catName + ' with ' + objKeywords[objKey])
+          for (let alts in altKeywords) {
+            const altKey = alts;
+            console.log('Now looking for alt keywords for ' + alts);
+            altKeywords[alts].forEach(function(oldTerm){
+              console.log('Checking for ' + oldTerm);
+            //   console.log('About to look for a match between ' + oldTerm + ' and ' + objKeywords[objKey]);
+            //
+            //   if (objKeywords[objKey] === oldTerm) {
+            //     console.log('Found a match between ' + keyword + ' and ' + oldTerm);
+            //   }
+            });
+          }
         }
       };
 
