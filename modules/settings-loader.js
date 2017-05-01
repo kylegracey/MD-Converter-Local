ac53cd44a826b4293ef2aae1ec9bb50af7adf31e
Kyle Gracey <kyle@kylegracey.com>
2017-05-01 14:27:42 -0500

    Additional details for goal
    Output format to add to settings object.

--- modules/settings-loader.js
+++ modules/settings-loader.js
@@ -1,1 +1,8 @@
 // Goal is to grab the text-file export of keywords from bridge, then load them in as settings for the MD Converter to work from.
+// Output should be formatted as:
+// {
+//   "KeywordCats" : {
+//     "Product" : ["Product 1","Product 2"],
+//     "Person" : ["Person 1, Person 2, Person 3"]
+//   }
+// }
