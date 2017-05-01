372e16aabf4342eb8cfe7ec6c2c5e890ab2a5511
Kyle Gracey <kyle@kylegracey.com>
2017-05-01 12:07:39 -0500

    Keyword Search test working
    Pulls in all specific keywords from settings.json, then searches
    through keywords on file and sorts them into the proper categories.
    
    Now need to add logic for non-matches and add them to a tags category.

--- temp.js
+++ temp.js
@@ -3,9 +3,32 @@ const getSetting = require('./modules/get-setting');
 
 const keywordCats = getSetting("KeywordCats");
 
+const tempObj = {
+  name: "tempObj",
+  keywords: "GTQ - Frost, JJ Watt, Recover Bar, Individual Product, Male, Efficacy, Football, One, Practice"
+};
+
 // Gets a specific setting by it's name
 (function(){
 
-  console.log(keywordCats);
+  for (let key in keywordCats) {
+    const catName = key;
+    const catSettingArr = keywordCats[key];
+    const objKeywords = tempObj.keywords.split(", ");
+    let catArrOutput = [];
+
+    catSettingArr.forEach(function(keyword){
+      // Look for matches between each keyword, and the keyword in the object.
+      for (let objKey in objKeywords){
+        if (keyword === objKeywords[objKey]) {
+          //Found an exact match!
+          catArrOutput.push(objKeywords[objKey]);
+        }
+      };
+
+    });
+
+    console.log("Category: " + catName + " | Keywords: " + catArrOutput);
+  }
 
 })();
