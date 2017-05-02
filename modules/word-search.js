405771f075e6c1c13e8b32a817cbc97d4933b797
Kyle Gracey <kyle@kylegracey.com>
2017-05-01 19:05:06 -0500

    Successfully finding matches with alt Keywords

--- modules/word-search.js
+++ modules/word-search.js
@@ -17,7 +17,7 @@ module.exports = function wordSearch(objInput, objOutput, KeywordCats){
 
   // List of Keywords to Re-map
   const altKeywords = {
-    "Whey Protein Bar" : ["Recover Bar", "Rec Bar"]
+    "Blender Bottle" : ["Shaker Bottle"]
   };
 
   for (let key in KeywordCats) {
@@ -30,24 +30,25 @@ module.exports = function wordSearch(objInput, objOutput, KeywordCats){
       for (let objKey in objKeywords){
         if (keyword === objKeywords[objKey]) {
           //Found an exact match!
-          console.log('Found exact match for ' + catName + ' with ' + objKeywords[objKey]);
+          // console.log('Found exact match for ' + catName + ' with ' + objKeywords[objKey]);
           catArrOutput.push(objKeywords[objKey]);
           objKeywords.splice(objKey, 1);
           break;
-        } else if (keyword !== objKeywords[objKey]) {
+        } else {
           // Check alt keywords
-          console.log("Didn't find match for " + catName + ' with ' + objKeywords[objKey])
+          // console.log("Didn't find match for " + catName + ' with ' + objKeywords[objKey])
           for (let alts in altKeywords) {
             const altKey = alts;
-            console.log('Now looking for alt keywords for ' + alts);
-            altKeywords[alts].forEach(function(oldTerm){
-              console.log('Checking for ' + oldTerm);
-            //   console.log('About to look for a match between ' + oldTerm + ' and ' + objKeywords[objKey]);
-            //
-            //   if (objKeywords[objKey] === oldTerm) {
-            //     console.log('Found a match between ' + keyword + ' and ' + oldTerm);
-            //   }
-            });
+            for (let altKeyword in altKeywords[alts]) {
+              const list = altKeywords[alts]
+              if (objKeywords[objKey] === list[altKeyword]) {
+                // catArrOutput.push(objKeywords[objKey]);
+                // objKeywords.splice(objKey, 1);
+              }
+            }
+            // console.log('Now looking for alt keywords for ' + alts);
+            // console.log('Trying to match ' + altKeywords[alts] + " and " + keyword);
+
           }
         }
       };
