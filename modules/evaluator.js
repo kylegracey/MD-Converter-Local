95d4130196e1de675aca5ae24b8e2c6ecbe615ce
Kyle Gracey <kyle@kylegracey.com>
2017-05-02 08:35:18 -0500

    Goal for branch
    Goal is to take the json output and verify it against all current terms
    before converting back to csv.

---
+++ modules/evaluator.js
@@ -0,0 +1,4 @@
+// Goal is to take the json output and verify it against all current terms before converting back to csv.
+// 1. Report back with a list of tags (including number of instances, and file paths).
+// 2. Report back with any new tags on existing categories due to bridge import
+// 3. Output report to json file to evaluate later 
